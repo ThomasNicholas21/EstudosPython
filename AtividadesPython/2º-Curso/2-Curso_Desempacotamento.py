22f398e36d0f634aa09adadca23364f6fd2bb16b
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-03 17:55:25 -0300

    Atividades

--- AtividadesPython/2º-Curso/2-Curso_Desempacotamento.py
+++ AtividadesPython/2º-Curso/2-Curso_Desempacotamento.py
@@ -1,1 +1,9 @@
 # Desempacotamento de lista
+
+lista = ['IDCliente', 'Senha', 'EMAIL', 'DADOS_IRRELEVANTES', 1, 2, 3, 4]
+I1, I2, I3, *_ = lista
+L1, L2, L3, *__ = _
+print(f'{I1} : {I2} : {I3} ')
+print(f' {_}')
+print(f'{L1} : {L2} : {L3}')
+print(f'{__}')
