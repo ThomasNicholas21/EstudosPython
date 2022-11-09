2c8881058a658fd5165f096ce967b6cd8bb4def8
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-09 17:37:21 -0300

    Atividade

---
+++ AtividadesPython/2º-Curso/2-Curso_Nome2.py
@@ -0,0 +1,25 @@
+"""
+Atividade verificação de nome
+"""
+while True:
+    nome = input('Digite seu nome aqui: ')
+
+    nome_curto = len(nome) <= 4
+    nome_normal = len(nome) >= 5 and len(nome) <= 6
+    nome_nulo = len(nome) == 0
+
+    if nome_nulo:
+        print('Insira seu nome novamente.')
+        continue
+    elif nome.isdigit():
+        print('Desculpe, você digitou um número, tente novamente.')
+        continue
+    elif nome_curto:
+        print(f'Seu nome, {nome}, é curto.')
+        break
+    elif nome_normal:
+        print(f'Seu nome, {nome}, é normal.')
+        break
+    else:
+        print(f'Seu nome, {nome}, é muito grande.')
+        break
