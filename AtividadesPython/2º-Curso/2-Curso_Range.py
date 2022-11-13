b6ac95a79d0b0fac2fb889aed627213264bbd4be
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-13 02:41:02 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Range.py
+++ AtividadesPython/2º-Curso/2-Curso_Range.py
@@ -2,7 +2,11 @@
 Utilizando o Range
 """
 
+from tracemalloc import start
+
+
 while True:
+    start = input('Digite aonde deseja começar a fazer sua contagem: ')
     stop = input('Digite até onde deseja contar seu numero: ')
     step = input(
                 'Digite como deseja a a forma que seus numeros sejam contados: '
@@ -14,9 +18,13 @@ while True:
     try:            
         stop_int = int(stop)
         step_int = int(step)
+        start_int = int(start)
 
-        print(f'Forma selecionada de {step_int} em {step_int}.')
-        for i in range(0, stop_int, step_int):
+        print(
+                f'Forma selecionada de {step_int} em {step_int}.'
+                f'\nE será contado de {start_int} até {stop_int}.'
+            )
+        for i in range(start_int, stop_int, step_int):
             cont += 1
             print(f'{cont}º número = {i}')
 
