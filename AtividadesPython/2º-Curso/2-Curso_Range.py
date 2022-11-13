a2fd2586c9075e391e7b3ded8b8c6c134b59cf92
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-13 00:18:27 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Range.py
+++ AtividadesPython/2º-Curso/2-Curso_Range.py
@@ -2,20 +2,25 @@
 Utilizando o Range
 """
 
-stop = input('Digite até onde deseja contar seu numero: ')
-step = input(
-            'Digite como deseja a a forma que seus numeros sejam contados: '
-            '\nExemplo: de 2 em 2 ou de 3 em 3 \n'
-            '\nComo deseja: '
-            )
+while True:
+    stop = input('Digite até onde deseja contar seu numero: ')
+    step = input(
+                'Digite como deseja a a forma que seus numeros sejam contados: '
+                '\nExemplo: de 2 em 2 ou de 3 em 3 \n'
+                '\nComo deseja: '
+                )
 
-try:            
-    stop_int = int(stop)
-    step_int = int(step)
+    try:            
+        stop_int = int(stop)
+        step_int = int(step)
 
-    for i in range(0, stop_int, step_int):
-        print(f'{i}')
+        for i in range(0, stop_int, step_int):
+            print(f'{i}')
 
-except:
-    print('Deve-se digitar apenas números!')
+    except:
+        print('Deve-se digitar apenas números!')
+        continue
     
+    sair = input('Deseja sair? [S]im ou [N]ão: ').lower().startswith('s')
+    if sair == True:
+        break
