c6a433d0ff1939f3c85519b8dfa6cf8be78970b3
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-12 23:22:49 -0300

    Atividade

---
+++ AtividadesPython/2º-Curso/2-Curso_Range.py
@@ -0,0 +1,21 @@
+"""
+Utilizando o Range
+"""
+
+stop = input('Digite até onde deseja contar seu numero: ')
+step = input(
+            'Digite como deseja a a forma que seus numeros sejam contados: '
+            '\nExemplo: de 2 em 2 ou de 3 em 3 \n'
+            '\nComo deseja: '
+            )
+
+try:            
+    stop_int = int(stop)
+    step_int = int(step)
+
+    for i in range(0, stop_int, step_int):
+        print(f'{i}')
+
+except:
+    print('Deve-se digitar apenas números!')
+    
