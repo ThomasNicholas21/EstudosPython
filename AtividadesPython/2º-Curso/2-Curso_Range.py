82cc6dfd9a4099425713fca0aabb32e5fc37d0b2
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-13 02:31:50 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Range.py
+++ AtividadesPython/2º-Curso/2-Curso_Range.py
@@ -6,16 +6,19 @@ while True:
     stop = input('Digite até onde deseja contar seu numero: ')
     step = input(
                 'Digite como deseja a a forma que seus numeros sejam contados: '
-                '\nExemplo: de 2 em 2 ou de 3 em 3 \n'
+                '\nExemplo: de 2 em 2 ou de 3 em 3 '
                 '\nComo deseja: '
                 )
+    cont = 0
 
     try:            
         stop_int = int(stop)
         step_int = int(step)
 
+        print(f'Forma selecionada de {step_int} em {step_int}.')
         for i in range(0, stop_int, step_int):
-            print(f'{i}')
+            cont += 1
+            print(f'{cont}º número = {i}')
 
     except:
         print('Deve-se digitar apenas números!')
