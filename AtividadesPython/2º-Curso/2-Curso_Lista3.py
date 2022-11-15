fb5cc98755c13081a2deae625125d241f6ed93a8
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-14 23:38:44 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Lista3.py
+++ AtividadesPython/2º-Curso/2-Curso_Lista3.py
@@ -15,6 +15,23 @@ while True:
 
     dado1, dado2, dado3 = dados_armazenados
 
+    deseja_retirar_algum_dado = input('Sim ou Não : ').lower().startswith('s')
+    if deseja_retirar_algum_dado is True:
+        qual_dado = input(
+                            'Qual dado deseja retirar: '
+                            f'\n{dado1}'
+                            f'\n{dado2}'
+                            f'\n{dado3}'
+                            '\nQual deseja: '
+                        )
+        if qual_dado == dado1:
+            dados_armazenados.pop(dado1)
+        elif qual_dado == dado2:
+            dados_armazenados.pop(dado2)
+        else:
+            dados_armazenados.pop(dado3)
+
+    print(dados_armazenados)
 
     sair = input('Sair? [S]im ou [N]ão: ').lower().startswith('s')
     if sair is True:
