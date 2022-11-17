f8363bb7f836460bbee46b73b2f7e89848422e1c
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-17 20:29:05 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Função4.py
+++ AtividadesPython/2º-Curso/2-Curso_Função4.py
@@ -3,9 +3,23 @@ Funções
 '''
 
 
-def saudacao(nome):
-    print(f'Olá, {nome}. ')
+def soma(a, b):
+    return a + b
 
 
-nome = input('Digite seu nome: ')
-saudacao(nome)
+def divisao(a, b):
+    return a / b
+
+
+def multiplicacao(a, b):
+    return a * b
+
+
+a = int(input('Digite a:'))
+b = int(input('Digite b: '))
+
+s = soma(a, b)
+d = divisao(a, b)
+m = multiplicacao(a, b)
+
+print(f'soma = {s}, divisão = {d}, multiplicação = {m}')
