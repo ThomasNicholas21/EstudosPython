8cb5c7cf702ef4ad882fa1d741cf6988a5e5e609
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-17 10:07:08 -0300

    Atividade

--- Desafios/2-CPF1.py
+++ Desafios/2-CPF1.py
@@ -2,9 +2,10 @@
 Validando CPF
 """
 
-# Primeiro Digito
+# Cálculo do Primeiro Digito
 
-cpf = input('Digite seu CPF: ')
+cpf = input('Digite seu CPF: ').replace('.', '') \
+    .replace('-', '')
 novo_cpf_1 = cpf[:9]
 total_soma_1 = 0
 reverso1 = 10
@@ -16,9 +17,7 @@ for i in novo_cpf_1:
 digito_1 = (total_soma_1 * 10) % 11
 digito_1 = digito_1 if digito_1 <= 9 else 0
 
-print(digito_1)
-
-# Segundo digito
+# Cálculo do Segundo digito
 
 novo_cpf_2 = novo_cpf_1 + str(digito_1)
 reverso2 = 11
@@ -30,8 +29,6 @@ for j in novo_cpf_2:
 
 digito_2 = (total_soma_2 * 10) % 11
 digito_2 = digito_2 if digito_2 <= 9 else 0
-
-print(digito_2)
 
 novo_cpf_3 = novo_cpf_2 + str(digito_2)
 
