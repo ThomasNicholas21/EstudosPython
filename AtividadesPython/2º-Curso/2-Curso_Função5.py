b63dd7ff7f2c7d39fb580bcc8f26aa7ef5885faf
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-18 17:28:43 -0300

    Atividade

--- AtividadesPython/2º-Curso/2-Curso_Função5.py
+++ AtividadesPython/2º-Curso/2-Curso_Função5.py
@@ -3,10 +3,54 @@ Função
 """
 
 
-def nome():
-    nome = input('Digite seu nome: ')
-    print(nome)
-    return nome
+def soma():
+    a = input('Digite o primeiro valor: ')
+    b = input('Digite o segundo valor: ')
+    a_int = int(a)
+    b_int = int(b)
+    print(f'Soma = {a_int + b_int}')
 
 
-nome()
+def subtracao():
+    a = input('Digite o primeiro valor: ')
+    b = input('Digite o segundo valor: ')
+    a_int = int(a)
+    b_int = int(b)
+    print(f'Subtração = {a_int - b_int}')
+
+
+def divisao():
+    a = input('Digite o primeiro valor: ')
+    b = input('Digite o segundo valor: ')
+    a_int = int(a)
+    b_int = int(b)
+    print(f'Divisão = {a_int / b_int}')
+
+
+def multiplicacao():
+    a = input('Digite o primeiro valor: ')
+    b = input('Digite o segundo valor: ')
+    a_int = int(a)
+    b_int = int(b)
+    print(f'Multiplicação = {a_int * b_int}')
+
+
+operador = input(
+    'Selecione seu operador: '
+    '\n+ Soma '
+    '\n- Subratração '
+    '\n/ Divisão '
+    '\n* Multiplicação '
+    'Selecione: '
+)
+
+if operador == '+':
+    soma()
+elif operador == '-':
+    subtracao()
+elif operador == '/':
+    divisao()
+elif operador == '*':
+    multiplicacao()
+else:
+    print('Digite um operador.')
