314c8f784f61afba768e8c70fdc7d5adc38c4104
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-21 22:20:37 -0300

    Atividade

--- EstudosPython/Curso-back-end/2º-Intermediário/Curso-Função.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Função.py
@@ -10,10 +10,13 @@ def multiplicacao(x, y):
 repetições = input('Quantas vezes deseja multiplicar: ')
 repetições_int = int(repetições)
 soma = 0
+contador = 0
 
 for i in range(repetições_int):
     x = int(input('Digite o primeiro valor: '))
     y = int(input('Digite o segundo valor: '))
     soma = soma + multiplicacao(x, y)
+    contador += 1
+    print(f'{contador}ª multiplicação = {multiplicacao(x, y)} \n')
 
-print(soma)
+print(f'soma total da multiplicação = {soma}')
