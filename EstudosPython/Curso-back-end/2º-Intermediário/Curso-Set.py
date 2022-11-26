dcb66796c31db354a7228264285aa237bcdea0ab
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-26 16:52:13 -0300

    ✔

--- EstudosPython/Curso-back-end/2º-Intermediário/Curso-Set.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Set.py
@@ -2,9 +2,14 @@
 trabalhando com set
 """
 
-set1 = set()
-set1.add('Thomas')
-set1.add(1)
-set1.update(('Hello World', 1, 2, 3, 4))
-set1.discard('Hello World')
-print(set1)
+# set1 = set()
+# set1.add('Thomas')
+# set1.add(1)
+# set1.update(('Hello World', 1, 2, 3, 4))
+# set1.discard('Hello World')
+# print(set1)
+
+A = {1, 2, 3}
+B = {4, 2, 5}
+c = A | B 
+print(c)
--- EstudosPython/Curso-back-end/Desafios/Desafio-Kinea.py
+++ EstudosPython/Curso-back-end/Desafios/Desafio-Kinea.py
@@ -2,17 +2,32 @@
 qual o 1337º numero primo
 """
 
-posicao = int(input('Digite a posicao: '))
-posicao_contador = 0
-n = 0
-contador = 0
-multiplo = 0
 
-while True:
-    n += 1
-    contador += 1
-    if n % contador == 0:
-        multiplo += 1
-
+def primo(n):
+    multiplo = 0
+    for numero in range(2,n):
+        if n % numero == 0:
+            multiplo += 1
     if multiplo == 0:
-        print(n)
+        return True
+    return False
+
+i = int(input('Digite qual posição do número primo: '))        
+lista = 0
+numero = 1
+
+while lista != i:
+    numero += 1
+    if primo(numero) is True:
+        lista += 1
+        if lista == i:
+            print(numero)
+        #lista.append(numero)
+    else:
+        continue
+
+#print(lista)
+
+
+
+#print(lista[i-1])
