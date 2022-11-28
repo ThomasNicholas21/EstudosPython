ecd39aab35d37eb5ee15ce4fabfac9f929930dcb
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-28 11:17:22 -0300

    ✔

--- EstudosPython/Curso-back-end/Desafios/Desafio-Kinea.py
+++ EstudosPython/Curso-back-end/Desafios/Desafio-Kinea.py
@@ -5,29 +5,23 @@ qual o 1337º numero primo
 
 def primo(n):
     multiplo = 0
-    for numero in range(2,n):
+    for numero in range(2, n):
         if n % numero == 0:
             multiplo += 1
     if multiplo == 0:
         return True
     return False
 
-i = int(input('Digite qual posição do número primo: '))        
-lista = 0
+
+posicao = int(input('Digite qual posição do número primo: '))
+numeros = 0
 numero = 1
 
-while lista != i:
+while numeros != posicao:
     numero += 1
     if primo(numero) is True:
-        lista += 1
-        if lista == i:
+        numeros += 1
+        if numeros == posicao:
             print(numero)
-        #lista.append(numero)
     else:
         continue
-
-#print(lista)
-
-
-
-#print(lista[i-1])
