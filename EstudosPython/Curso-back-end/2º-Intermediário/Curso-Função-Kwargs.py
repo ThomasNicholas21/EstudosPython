2026e2cc2ad6604fb972815c7b31a4b6cd749c15
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-11-29 17:43:46 -0300

    Utilizando Kwargs

---
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Função-Kwargs.py
@@ -0,0 +1,28 @@
+"""
+Utilizando Kwargs como visto na aula
+"""
+
+
+def argumentos_nomeados(**kwargs):
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+    print()
+
+
+pessoa_dados_principais = {
+    'Nome': 'Thomas',
+    'Idade': '22',
+    'email': 'thomasnicholaas@gmail.com',
+    'ocupação': 'estudante',
+    'Aonde mora': 'Anápolis',
+}
+
+pessoa_dados_secundarios = {
+    'Hobbie': 'Tocar violão',
+    'Frequenta academia': 'Sim',
+    'Assisti séries': 'Sim',
+    'Comida favorita': 'Hamburguer',
+}
+
+argumentos_nomeados(**pessoa_dados_principais)
+argumentos_nomeados(**pessoa_dados_secundarios)
