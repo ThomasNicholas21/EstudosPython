b222ae7b73fb24667adc42477c8cf385ae0647e3
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-12-01 17:09:06 -0300

    generator expression, iterables e iterators

--- EstudosPython/Curso-back-end/2º-Intermediário/Curso-iterables-iterators.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-iterables-iterators.py
@@ -1,13 +1,24 @@
 """
-iterables e iterators
+generator expression, iterables e iterators
 """
 # iterators fornece uma maneira de acessar
 # sequencialmente os elementos de um objeto
 # agregado sem expor sua representação subjacente
+import sys
 
 iterable = ['Thomas', 'Nicholas', '__iter__']
 iterator = iter(iterable)
+lista = [n for n in range(1000)]
+gerador = (n for n in range(1000))  # Generator expression
+# generator é uma função que sabe pausar
+# necessita do next para funcionar
 
-print(next(iterator))
-print(next(iterator))
-print(next(iterator))
+# getsizeof pega o tamanho
+print(sys.getsizeof(lista))
+print(sys.getsizeof(gerador))
+# print(next(iterator))
+# print(next(iterator))
+# print(next(iterator))
+
+# for n in gerador:
+#     print(n)
