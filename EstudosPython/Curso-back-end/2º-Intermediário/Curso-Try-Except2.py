43102125f932c42848f07652cb0ca8d935910933
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-12-02 12:02:48 -0300

    Try e Except

---
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Try-Except2.py
@@ -0,0 +1,24 @@
+"""
+TRY e EXCEPT
+"""
+
+# a = int(input('A: '))
+# b = int(input('B: '))
+# c = a / b
+
+try:
+    a = int(input('A: '))
+    b = int(input('B: ')) 
+    c = a / b
+    print(c)
+except ZeroDivisionError as error:
+    print('MSG: ', error.__class__.__name__)
+
+except ValueError:
+    print('Valor invalido')
+
+except (TypeError, IndexError):
+    print('Erros: tipo e index')
+
+except Exception:
+    print('Erro desconhecido')
