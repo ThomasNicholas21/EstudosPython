7d0992ee12a0da7868dbe40349fbb3e535457b0b
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-12-04 23:02:18 -0300

    Atividade aplicando módulo

---
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Módulo-Atividade1.py
@@ -0,0 +1,61 @@
+"""
+Atividade com módulo
+"""
+import os
+from Curso1 import somar, subtrair, dividir, multiplicar
+
+armazenamento = []
+while True:   
+    a = input('Digite o primeiro número: ')
+    b = input('Digite o segundo número: ')
+
+    try:
+        a = int(a)
+        b = int(b)
+
+    except ValueError:
+        print('Valor invalido!')
+        continue
+
+    except Exception:
+        print('Algo deu errado, tente novamente!')
+        continue
+    
+    print()
+    operador = input(
+        '| Digite seu operador:|\n'
+        '| [+] - Soma          |\n'
+        '| [-] - Subtração     |\n'
+        '| [/] - Divisão       |\n'
+        '| [*] - Multiplicação |\n'
+        'Qual deseja: '
+        ).lower()
+    
+    if operador == '+':
+        soma = somar(a, b)
+        print(f'{a} + {b} = ', soma)
+        armazenamento.append(soma)
+    elif operador == '-':
+        subtracao = subtrair(a, b)
+        print(f'{a} - {b} = ', subtracao)
+        armazenamento.append(subtracao)
+    elif operador == '/':
+        divisao = dividir(a, b)
+        print(f'{a} / {b} = ', divisao)
+        armazenamento.append(divisao)
+    elif operador == '*':
+        multiplicacao = multiplicar(a, b)
+        print(f'{a} x {b} = ', multiplicacao)
+        armazenamento.append(multiplicacao)
+    else:
+        print('Operação inválida tente novamente!')
+        continue
+    
+    print('Valores finais operados: ', armazenamento)
+    print()
+    sair = input('Deseja sair? [S]im ou [N]ão: ').lower().startswith('s')
+    if sair is True:
+        break
+    else:
+        os.system('cls')
+    
---
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso1.py
@@ -0,0 +1,20 @@
+"""
+Fazendo funções de leitura
+"""
+
+def somar(a, b):
+    soma = a + b
+    return soma
+
+def subtrair(a, b):
+    subtracao = a - b
+    return subtracao
+
+def dividir(a, b):
+    divisao = a / b
+    return round(divisao, 2)
+
+def multiplicar(a, b):
+    multiplicacao = a * b
+    return multiplicacao
+
