e5c3b21001722882b3f650bc01761762ec2a63db
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-12-06 13:48:28 -0300

    Atividade proposta

---
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Exercício-Proposto.py
@@ -0,0 +1,53 @@
+# copy, sorted, produtos.sort
+# Exercícios
+# Aumente os preços dos produtos a seguir em 10%
+# Gere novos_produtos por deep copy (cópia profunda)
+
+# produtos = [
+#     {'nome': 'Produto 5', 'preco': 10.00},
+#     {'nome': 'Produto 1', 'preco': 22.32},
+#     {'nome': 'Produto 3', 'preco': 10.11},
+#     {'nome': 'Produto 2', 'preco': 105.87},
+#     {'nome': 'Produto 4', 'preco': 69.90},
+# ]
+
+# Ordene os produtos por nome decrescente (do maior para menor)
+# Gere produtos_ordenados_por_nome por deep copy (cópia profunda)
+
+# Ordene os produtos por preco crescente (do menor para maior)
+# Gere produtos_ordenados_por_preco por deep copy (cópia profunda)
+
+import copy
+
+produtos = [
+    {'nome': 'Produto 1', 'preco': 12.50},
+    {'nome': 'Produto 2', 'preco': 22.12},
+    {'nome': 'Produto 5', 'preco': 10.66},
+    {'nome': 'Produto 4', 'preco': 120.22},
+    {'nome': 'Produto 3', 'preco': 88.36},
+]
+
+produtos = [
+    {**produto, 'preco': round(produto['preco'] * 1.10, 2)}
+    for produto in produtos
+]
+
+novos_produtos = copy.deepcopy(produtos) # copia profunda dos dados modificados
+
+print('Novos precos: ')
+print(*novos_produtos, sep='\n')
+print()
+
+produtos = sorted(produtos, key=lambda produto: produto['nome'], reverse=True)
+
+produtos_ordenados_por_nome = copy.deepcopy(produtos)
+print('Ordenando por nome: ')
+print(*produtos_ordenados_por_nome, sep='\n')
+print()
+
+produtos = sorted(produtos, key=lambda produto: produto['preco'])
+
+produtos_ordenados_por_preco = copy.deepcopy(produtos)
+print('Ordenando por preco: ')
+print(*produtos_ordenados_por_preco, sep='\n')
+print()
