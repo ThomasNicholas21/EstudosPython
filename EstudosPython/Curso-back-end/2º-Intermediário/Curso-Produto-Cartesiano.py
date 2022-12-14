ae276601a5d5de9ee9b916150176b41b48ed2535
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2022-12-14 20:25:45 -0300

    Permutação, combinação e produto

--- EstudosPython/Curso-back-end/2º-Intermediário/Curso-Produto-Cartesiano.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/Curso-Produto-Cartesiano.py
@@ -1,12 +1,24 @@
 """
 produto cartesiano
 """
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+from itertools import combinations, permutations, product
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
 
 conjuntoA = [1, 3, 4]
 conjuntoB = [2, 3]
-
+conjuntoAB = [
+    [1, 3, 4],
+    [2, 3]
+]
 for i in conjuntoA:
-    for j in conjuntoB:
+    for j in conjuntoB: 
         print(f'({i}, {j})')
 
 print()
@@ -18,3 +30,10 @@ print()
 for i in conjuntoB:
     for j in conjuntoB:
         print(f'({i}, {j})')
+
+print()
+print_iter(combinations(conjuntoA, 2))  
+print_iter(combinations(conjuntoB, 2))  
+print_iter(permutations(conjuntoA, 2))  
+print_iter(permutations(conjuntoB, 2))    
+print_iter(product(*conjuntoAB))
