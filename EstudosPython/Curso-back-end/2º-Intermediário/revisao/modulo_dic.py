897ac9274155b3331ead9279e7267aae02c02d11
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2023-01-06 16:06:06 -0300

    finalizando revisão

--- EstudosPython/Curso-back-end/2º-Intermediário/revisao/modulo_dic.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/revisao/modulo_dic.py
@@ -1,11 +1,14 @@
 
 produtos = {
         'Perfume': 'Calvin Klein',
-        'Valor'  : 'R$150,00',
+        'Valor 1'  : 'R$150,00',
+        
         'Celular': 'Samsung Galaxy S21',
-        'Valor'  : 'R$5250,00',
+        'Valor 2'  : 'R$5250,00',
+
         'Mouse'  : 'Logitech',
-        'Valor'  : 'R$250,00',
+        'Valor 3'  : 'R$250,00',
+
         'Monitor': 'Acer 144Hz',
-        'Valor'  : 'R$1500,00',
+        'Valor 4'  : 'R$1500,00',
     }
--- EstudosPython/Curso-back-end/2º-Intermediário/revisao3.py
+++ EstudosPython/Curso-back-end/2º-Intermediário/revisao3.py
@@ -1,6 +1,8 @@
 # Revisando módulo
 from revisao import produtos
+from os import system
 
+carrinho_de_compra = []
 while True:
     escolha = input(
         'Produtos disponiveis:\n'
@@ -11,15 +13,43 @@ while True:
         'Qual das opcoes deseja: '
     )
     print()
-
+    
     if escolha == '1':
         print(produtos['Perfume'])
+        print('O valor é de:', produtos['Valor 1'])
+        print()
     elif escolha == '2':
         print(produtos['Celular'])
+        print('O valor é de:', produtos['Valor 2'])
+        print()
     elif escolha == '3':
         print(produtos['Mouse'])
+        print('O valor é de:', produtos['Valor 3'])
+        print()
     elif escolha == '4':
         print(produtos['Monitor'])
+        print('O valor é de:', produtos['Valor 4'])
+        print()
     else:
         print('Um erro ocorreu, tente novamente.')
+
+    compra = input(
+        'Deseja colocar no carrinho de compra? [S]im ou [N]ão: '
+    ).lower().startswith('s')
+    if compra is True:
+        carrinho_de_compra.append(escolha)
+        print('Produto encaminhado para o seu Carrinho de compras 😀')
+        system('cls')
+    
+    sair = input(
+            'Deseja realizar a compra e sair? [S]im ou [N]ão: '
+    ).lower().startswith('s')
+    if sair is True:
+        print('Compra(s) realizada(s) com sucesso!')
+        print(f'Produtos adquiridos: {carrinho_de_compra}')
+        break
+    else:
+        system('cls')
+        print(f'Produtos: {carrinho_de_compra}, estão no carrinho.')
+        continue
     
