9f5f3edabade9c8b2a7fc8e30a120651c1b38445
Thomas Nicholas Pedrosa Matias <thomasnicholaas@gmail.com>
2023-05-11 17:57:50 -0300

    Atividade proposta lista - continuacao

--- EstudosPython/Curso-back-end/Desafios/Curso-Lista-Tarefas.py
+++ EstudosPython/Curso-back-end/Desafios/Curso-Lista-Tarefas.py
@@ -1,18 +1,45 @@
-listar = []
-desfazer = []
+def listar(tarefas):
+    print()
+    if not tarefas:
+        print('Nenhuma tarefa listada.')
+        return
+    
+    print('Tarefas:')
+    for tarefa in tarefas:
+        print(f'\t{tarefa}')
+    print()
+
+def desfazer():
+    print('Tarefa desfeita.\n')
+    return tarefas_desfazer.append(tarefas.pop())
+
+def refazer():
+    print('Tarefa restaurada.\n')
+    return tarefas.append(tarefas_desfazer.pop())
+
+def adicionar(tarefa, tarefas):
+    tarefa = tarefa.strip()
+    if not tarefas:
+        print('Isso não é uma tarefa.')
+        return
+    
+    tarefas.append(tarefa)
+
+tarefas = []
+tarefas_desfazer = []
 
 while True:
 
-    print('Comandos: Listar, desfazer e refazer.')
-    op = input('Digite a tarefa ou comando: ').lower()
-    print()
+    print('Comandos: Listar, desfazer, refazer e sair.')
+    op = input('Digite a tarefa ou comando: ')
 
     if op == 'listar':
-        print('Tarefas:\n', *listar, sep='\n')
-        print()
+        listar(tarefas)
     elif op == 'desfazer':
-        desfazer.append(listar.pop())
+        desfazer()
     elif op == 'refazer':
-        listar.append(desfazer.pop())
+        refazer()
+    elif op == 'sair':
+        break
     else:
-        listar.append(op)
+        adicionar(op, tarefas)
