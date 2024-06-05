715c88179cf9136965fffe014a9058c49985b21f
ThomasNicholas21 <thomasnicholaas@gmail.com>
2024-06-04 21:23:19 -0300

    Atribuindo novos requisitos (revisão)

--- EstudosPython/revisao/intermediario/rev23.py
+++ EstudosPython/revisao/intermediario/rev23.py
@@ -5,24 +5,49 @@
 
 
 # Criando funcoes 
-def add_product(estoque, name, amount, price):
+def add_product(stock, name, amount, price):
     if amount < 0 or price < 0:
-        raise ValueError("A quantidade e o preco deve ser maior que zero!!")
-    if name in estoque:
-        estoque[name]["Quantidade"] += amount
-        estoque[name]["Preco"] = price
+        raise ValueError("The amount or price has to be more tn zero!")
+    if name in stock:
+        stock[name]["Amount"] += amount
+        stock[name]["Price"] = price
     else:
-        estoque[name] = {"Quantidade": amount, "Preco": price}
+        stock[name] = {"Amount": amount, "Price": price}
 
 
-def remove_product():
-    ...
+def remove_product(stock, name, amount):
+    if name not in stock:
+        raise KeyError("This product is not in stock!")
+    if amount > stock[name]["Amount"]:
+        raise ValueError("This is more than we have in stock!")
+    
+    stock[name]["Amount"] -= amount
 
-def find_product():
-    ...
+    if stock[name]["Amount"] == 0:
+        del stock[name]
 
-estoque = {}
-add_product(estoque, "maçã", 10, 1.5)
-add_product(estoque, "banana", 25, 3)
-add_product(estoque, "maçã", 15, 1.5)
-print(list(estoque.items()))
+
+def find_product(stock, name):
+    if name not in stock:
+        raise KeyError("We don't have this product!")
+    else:
+        print(list(*stock.items()))
+    
+
+stock = {}
+add_product(stock, "apple", 10, 1.5)
+add_product(stock, "banana", 25, 3)
+add_product(stock, "apple", 15, 1.5)
+for key, item in stock.items():
+    print(key, item)
+
+remove_product(stock, "apple", 5)
+remove_product(stock, "banana", 25)
+#remove_product(stock, "pineapple", 1)
+#remove_product(stock, "apple", 25)
+
+for key, item in stock.items():
+    print(key, item)
+
+find_product(stock, "apple")
+find_product(stock, "pineapple")
